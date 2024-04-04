a90c74928b74a3a3050ded1cf08c8e2e55cce8f6
Thomas Gruber <thomas.gruber@fau.de>
2024-04-04 10:48:44 +0200

    Update stack implementation

--- include/stack.h
+++ include/stack.h
@@ -5,8 +5,10 @@ typedef struct
 	int top;
 } Stack;
 
-void stackInit(Stack *s, int size);
-void stackPush(Stack *s, void* val);
+#define FRESH_STACK {NULL, 0, -1}
+
+int stackInit(Stack *s, int size);
+int stackPush(Stack *s, void* val);
 void* stackTop(Stack *s);
 void* stackPop(Stack *s);
 int stackSize(Stack *s);
