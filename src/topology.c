c42b6d66ad63307501c7fde8711a4abfda93c3cc
Thomas Roehl <thomas.roehl@fau.de>
2024-08-27 15:44:52 +0200

    Use minimum length when adding to output list

--- src/topology.c
+++ src/topology.c
@@ -932,7 +932,7 @@ int cpustr_to_cpulist_logical(bstring cpustr, int* list, int length)
     }
 
     int outcount = 0;
-    for (int i = 0; i < idxLen && i < length; i++)
+    for (int i = 0; i < looplength && outcount < length; i++)
     {
         list[outcount++] = tmpList[idxList[i]];
     }
