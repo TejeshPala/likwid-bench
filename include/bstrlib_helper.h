5fffdccfd2b1c9023fff673dbbbfd1f151d5ba38
Thomas Roehl <Thomas.Roehl@googlemail.com>
2024-08-30 18:08:00 +0200

    Add function to sort bstrList and some initial tests

--- include/bstrlib_helper.h
+++ include/bstrlib_helper.h
@@ -19,16 +19,21 @@ struct bstrList* bstrListCopy(struct bstrList * sl);
 int bstrListRemove(struct bstrList * sl, bstring str);
 
 void bstrListPrint(struct bstrList * sl);
+int bstrListSort(struct bstrList* in, struct bstrList** out);
+typedef int (*bstrListSortFunction)(const struct tagbstring * left, const struct tagbstring * right);
+int bstrListSortFunc(struct bstrList* in, bstrListSortFunction cmp, struct bstrList** out);
+
 
 int btrimbrackets (bstring b);
 int bisnumber(bstring b);
-
-bstring read_file(char *filename);
+int bisinteger(bstring b);
 
 int batoi(bstring b, int* value);
 int batoi64(bstring b, int64_t* value);
 int batof(bstring b, float* value);
 int batod(bstring b, double* value);
+
+bstring read_file(char *filename);
 
 #ifdef __cplusplus
 }
