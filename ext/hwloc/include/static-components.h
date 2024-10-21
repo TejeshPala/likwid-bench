8f6058e0213555484effbb0850973951bfef708c
Thomas Roehl <Thomas.Roehl@googlemail.com>
2024-10-21 17:30:12 +0200

    hwloc: use hwloc_x86_component on x86 and x86_64 systems

--- ext/hwloc/include/static-components.h
+++ ext/hwloc/include/static-components.h
@@ -5,6 +5,8 @@ static const struct hwloc_component * hwloc_static_components[] = {
   &hwloc_synthetic_component,
   &hwloc_xml_nolibxml_component,
   &hwloc_linux_component,
+#if defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
   &hwloc_x86_component,
+#endif
   NULL
 };
