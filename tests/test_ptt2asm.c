358e282aa567f811601e67b91f010e3df8fb9b12
Tejesh Pala <tejesh.pala@fau.de>
2024-11-17 01:52:50 +0100

    fix failed test for ppt2asm
    Signed-off-by: Tejesh Pala <tejesh.pala@fau.de>

--- tests/test_ptt2asm.c
+++ tests/test_ptt2asm.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "allocator.h"
 #include "error.h"
 #include "bstrlib.h"
 #include "bstrlib_helper.h"
@@ -11,16 +12,30 @@
 
 int global_verbosity = DEBUGLEV_DEVELOP;
 
-
 int main(int argc, char* argv[])
 {
     struct bstrList* out = bstrListCreate();
+    struct bstrList* regs = bstrListCreate();
+    RuntimeConfig runcfg;
+    memset(&runcfg, 0, sizeof(RuntimeConfig));
     TestConfig tcfg;
     memset(&tcfg, 0, sizeof(TestConfig));
+    static struct tagbstring default_stream_name = bsStatic("STR0");
+    int ival = 10;
+    tcfg.num_streams = 1;
+    tcfg.streams = (TestConfigStream*)malloc(sizeof(TestConfigStream) * tcfg.num_streams);
+    tcfg.streams[0] = (TestConfigStream){
+        .name = &default_stream_name,
+        .num_dims = 1,
+        .type = TEST_STREAM_TYPE_INT,
+        .btype = NULL,
+        .data = {ival},
+        };
     tcfg.name = bfromcstr("doubleload");
     tcfg.code = bfromcstr("xor xmm0, xmm0\nLOOP(loop, rax=0, <, rdi=N, UNROLL_FACTOR)\nxor xmm0, xmm0\nDUMMY(myunroll)\nmovsd    xmm0, [STR0 + rax * 8]\nmovsd    xmm1, [STR0 + rax * 8 + 8]\nmovsd    xmm2, [STR0 + rax * 8 + 16]\nmovsd    xmm3, [STR0 + rax * 8 + 24]\nmovsd    xmm4, [STR0 + rax * 8 + 32]\nmovsd    xmm5, [STR0 + rax * 8 + 40]\nmovsd    xmm6, [STR0 + rax * 8 + 48]\nmovsd    xmm7, [STR0 + rax * 8 + 56]\nDUMMYEND(myunroll)\nxor xmm0, xmm0\nLOOPEND(loop)\nxor xmm0, xmm0\nxor xmm0, xmm0\nLOOP(loop2, rax=0, <, rdi=N, UNROLL_FACTOR)\nxor xmm0, xmm0\nDUMMY(myunroll2)\nmovsd    xmm0, [STR0 + rax * 8]\nmovsd    xmm1, [STR0 + rax * 8 + 8]\nmovsd    xmm2, [STR0 + rax * 8 + 16]\nmovsd    xmm3, [STR0 + rax * 8 + 24]\nmovsd    xmm4, [STR0 + rax * 8 + 32]\nmovsd    xmm5, [STR0 + rax * 8 + 40]\nmovsd    xmm6, [STR0 + rax * 8 + 48]\nmovsd    xmm7, [STR0 + rax * 8 + 56]\nDUMMYEND(myunroll2)\nxor xmm0, xmm0\nLOOPEND(loop2)\nxor xmm0, xmm0\n");
     //tcfg.code = bfromcstr("xor xmm0, xmm0\nLOOP(loop, rax=0, <, rdi=N, UNROLL_FACTOR)\nxor xmm0, xmm0\nDUMMY(myunroll)\nmovsd    xmm0, [STR0 + rax * 8]\nmovsd    xmm1, [STR0 + rax * 8 + 8]\nmovsd    xmm2, [STR0 + rax * 8 + 16]\nmovsd    xmm3, [STR0 + rax * 8 + 24]\nmovsd    xmm4, [STR0 + rax * 8 + 32]\nmovsd    xmm5, [STR0 + rax * 8 + 40]\nmovsd    xmm6, [STR0 + rax * 8 + 48]\nmovsd    xmm7, [STR0 + rax * 8 + 56]\nDUMMYEND(myunroll)\nxor xmm0, xmm0\nLOOPEND(loop)\n");
-    prepare_ptt(&tcfg, out);
+    runcfg.tcfg = &tcfg;
+    prepare_ptt(&tcfg, out, regs);
     printf("Body:\n");
     for (int i = 0; i < out->qty; i++)
     {
@@ -28,7 +43,7 @@ int main(int argc, char* argv[])
     }
     bstrListDestroy(out);
     out = bstrListCreate();
-    generate_code(&tcfg, out);
+    generate_code(&runcfg, out);
     printf("Final:\n");
     for (int i = 0; i < out->qty; i++)
     {
@@ -36,6 +51,8 @@ int main(int argc, char* argv[])
     }
     bdestroy(tcfg.code);
     bdestroy(tcfg.name);
+    bstrListDestroy(regs);
     bstrListDestroy(out);
+    free(tcfg.streams);
     return 0;
 }
